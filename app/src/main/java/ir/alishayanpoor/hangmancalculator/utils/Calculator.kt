237307278cd04c5a1f1b79425e684c47c81222dd
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-19 18:50:09 +0430

    Create Calculator.kt

---
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/utils/Calculator.kt
@@ -0,0 +1,12 @@
+package ir.alishayanpoor.hangmancalculator.utils
+
+object Calculator {
+    /*fun calc(rawText: String): String {
+
+    }*/
+
+    fun add(num1: Int, num2: Int) = num1 + num2
+    fun sub(num1: Int, num2: Int) = num1 - num2
+    fun div(num1: Int, num2: Int) = num1 / num2
+    fun mul(num1: Int, num2: Int) = num1 * num2
+}
