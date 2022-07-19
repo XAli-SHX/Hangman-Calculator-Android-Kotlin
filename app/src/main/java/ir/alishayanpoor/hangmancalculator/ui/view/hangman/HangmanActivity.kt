65168aa21a24fed22147e4d67be5e288c59c042c
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-19 22:13:00 +0430

    Create base activity to test next state

--- app/src/main/java/ir/alishayanpoor/hangmancalculator/ui/view/hangman/HangmanActivity.kt
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/ui/view/hangman/HangmanActivity.kt
@@ -1,41 +1,77 @@
 package ir.alishayanpoor.hangmancalculator.ui.view.hangman
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ir.alishayanpoor.hangmancalculator.ui.theme.HangmanCalculatorTheme
+import androidx.compose.ui.res.painterResource
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import ir.alishayanpoor.hangmancalculator.utils.exhaustive
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class HangmanActivity : ComponentActivity() {
+    private val viewModel by viewModels<HangmanViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        subscribeObservers()
         setContent {
-            HangmanCalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            HangmanScreen()
+        }
+    }
+
+    @Suppress("IMPLICIT_CAST_TO_ANY")
+    private fun subscribeObservers() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.event.receiveAsFlow().collectLatest {
+                when (it) {
+                    is HangmanUiEvent.Error -> showError(it)
+                    is HangmanUiEvent.GameOver -> gameOver()
+                }.exhaustive
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    private fun gameOver() {
+        Toast.makeText(this@HangmanActivity,
+            "You missed the calculation!",
+            Toast.LENGTH_SHORT).show()
+        Toast.makeText(this@HangmanActivity,
+            "BYE!",
+            Toast.LENGTH_SHORT).show()
+        lifecycleScope.launch {
+            delay(5000)
+            finishAffinity()
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    HangmanCalculatorTheme {
-        Greeting("Android")
+    private fun showError(it: HangmanUiEvent.Error) {
+        Toast.makeText(this@HangmanActivity,
+            it.message,
+            Toast.LENGTH_SHORT).show()
+    }
+
+    @Composable
+    private fun HangmanScreen() {
+        Column(Modifier.fillMaxSize()) {
+            Image(painterResource(id = viewModel.getResourceByState()),
+                "state1 - Initialize of hangman")
+            Button(onClick = { viewModel.nextState() }) {
+                Text(text = "Next state")
+            }
+        }
     }
 }
--- app/src/main/java/ir/alishayanpoor/hangmancalculator/ui/view/hangman/HangmanViewModel.kt
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/ui/view/hangman/HangmanViewModel.kt
@@ -1,9 +1,41 @@
 package ir.alishayanpoor.hangmancalculator.ui.view.hangman
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import ir.alishayanpoor.hangmancalculator.domain.repo.HangmanRepo
+import ir.alishayanpoor.hangmancalculator.domain.use_case.HangmanUseCase
+import ir.alishayanpoor.hangmancalculator.exception.AppException
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class HangmanViewModel : ViewModel() {
+@HiltViewModel
+class HangmanViewModel @Inject constructor(
+    private val hangmanUseCase: HangmanUseCase,
+    private val repo: HangmanRepo,
+) : ViewModel() {
+    var state by mutableStateOf(HangmanUiState())
+        private set
+    val event = Channel<HangmanUiEvent>()
 
+    fun nextState() {
+        try {
+            state = state.copy(
+                currentState = repo.nextState(state.currentState)
+            )
+            if (state.currentState == HangmanState.S8End)
+                viewModelScope.launch {
+                    event.send(HangmanUiEvent.GameOver)
+                }
+        } catch (e: AppException) {
+            viewModelScope.launch { event.send(HangmanUiEvent.Error(e.localizedMessage)) }
+        }
+    }
 
+    fun getResourceByState() = repo.getStateImageResourceByCurrentState(state.currentState)
 
 }
