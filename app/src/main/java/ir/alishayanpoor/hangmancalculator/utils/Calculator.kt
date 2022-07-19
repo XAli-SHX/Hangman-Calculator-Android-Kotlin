1af7bcc19b46a161faa58c5570e1816fe9b689eb
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-19 19:25:10 +0430

    Complete calculator

--- app/src/main/java/ir/alishayanpoor/hangmancalculator/utils/Calculator.kt
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/utils/Calculator.kt
@@ -1,12 +1,55 @@
 package ir.alishayanpoor.hangmancalculator.utils
 
+import ir.alishayanpoor.hangmancalculator.exception.AppException
+
 object Calculator {
-    /*fun calc(rawText: String): String {
+    @Throws(AppException::class)
+    fun calc(rawText: String): String {
+        var op: String? = null
+        val text = rawText.removeWhiteSpaces()
+        text.forEach { char ->
+            op = getArithmeticList().find { it == char.toString() }
+            op?.let { return@forEach }
+        }
+        op?.let {
+            return when (it) {
+                Constants.CALC_BUTTON_SUM -> add(extractNumbers(text, it))
+                Constants.CALC_BUTTON_SUB -> sub(extractNumbers(text, it))
+                Constants.CALC_BUTTON_DIV -> div(extractNumbers(text, it))
+                Constants.CALC_BUTTON_MUL -> mul(extractNumbers(text, it))
+                else -> throw AppException("Not supported yet")
+            }.toString()
+        } ?: throw AppException("Not supported yet")
+    }
 
-    }*/
+    fun extractNumbers(
+        rawText: String,
+        op: String,
+    ): Pair<Int, Int> {
+        val numData = rawText.split(op)
+        val num1 = numData[0].toInt()
+        val num2 = numData[1].toInt()
+        return Pair(num1, num2)
+    }
 
-    fun add(num1: Int, num2: Int) = num1 + num2
-    fun sub(num1: Int, num2: Int) = num1 - num2
-    fun div(num1: Int, num2: Int) = num1 / num2
-    fun mul(num1: Int, num2: Int) = num1 * num2
+    fun add(numbers: Pair<Int, Int>) = numbers.first + numbers.second
+    fun sub(numbers: Pair<Int, Int>) = numbers.first - numbers.second
+
+    @Throws(AppException::class)
+    fun div(numbers: Pair<Int, Int>): Int {
+        if (numbers.second == 0)
+            throw AppException("Divide by ZERO is not accepted")
+        return numbers.first / numbers.second
+    }
+
+    fun mul(numbers: Pair<Int, Int>) = numbers.first * numbers.second
+
+    fun getArithmeticList(): List<String> {
+        return listOf(
+            Constants.CALC_BUTTON_DIV,
+            Constants.CALC_BUTTON_MUL,
+            Constants.CALC_BUTTON_SUB,
+            Constants.CALC_BUTTON_SUM,
+        )
+    }
 }
