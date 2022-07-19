02baffb5d67c12f63b392a418d2aaca8d868fcf2
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-20 00:05:57 +0430

    Update HangmanUseCase.kt

--- app/src/main/java/ir/alishayanpoor/hangmancalculator/domain/use_case/HangmanUseCase.kt
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/domain/use_case/HangmanUseCase.kt
@@ -1,5 +1,36 @@
 package ir.alishayanpoor.hangmancalculator.domain.use_case
 
-class HangmanUseCase {
+import ir.alishayanpoor.hangmancalculator.domain.repo.HangmanRepo
+import ir.alishayanpoor.hangmancalculator.exception.AppException
+import ir.alishayanpoor.hangmancalculator.ui.view.hangman.HangmanState
+import javax.inject.Inject
 
+class HangmanUseCase @Inject constructor(
+    private val repo: HangmanRepo,
+) {
+    fun nextState(currentState: HangmanState): HangmanState {
+        return repo.nextState(currentState)
+    }
+
+    fun getStateImageResourceByCurrentState(currentState: HangmanState): Int {
+        return repo.getStateImageResourceByCurrentState(currentState)
+    }
+
+    fun getStateImageDescriptionByCurrentState(currentState: HangmanState): String {
+        return repo.getStateImageDescriptionByCurrentState(currentState)
+    }
+
+    fun checkEnteredNumber(calcResult: String, number: String): List<Int> {
+        var hasBeenFound = false
+        val indexList = mutableListOf<Int>()
+        calcResult.forEachIndexed { index, c ->
+            if (c.toString() == number) {
+                hasBeenFound = true
+                indexList.add(index)
+            }
+        }
+        if (!hasBeenFound)
+            throw AppException("No number found in calculation")
+        return indexList.toList()
+    }
 }
