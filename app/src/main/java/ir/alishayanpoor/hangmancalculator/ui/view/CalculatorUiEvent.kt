9522eea5be0dbe60111ddd0365162dec678b3e9a
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-19 19:38:39 +0430

    Add result data to navigate to hangman

--- app/src/main/java/ir/alishayanpoor/hangmancalculator/ui/view/CalculatorUiEvent.kt
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/ui/view/CalculatorUiEvent.kt
@@ -1,6 +1,6 @@
 package ir.alishayanpoor.hangmancalculator.ui.view
 
 sealed class CalculatorUiEvent {
-    object StartHangman : CalculatorUiEvent()
+    data class StartHangman(val result: String) : CalculatorUiEvent()
     data class Error(val message: String) : CalculatorUiEvent()
 }
