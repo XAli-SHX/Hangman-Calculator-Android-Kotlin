60214af792810c91a4ad6d80b37d114e9737057a
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-19 22:32:19 +0430

    Add some navigation helper functions
    Add launchActivity and getNavigationStringData

--- app/src/main/java/ir/alishayanpoor/hangmancalculator/utils/Navigator.kt
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/utils/Navigator.kt
@@ -1,4 +1,29 @@
 package ir.alishayanpoor.hangmancalculator.utils
 
-class Navigator {
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import ir.alishayanpoor.hangmancalculator.exception.AppException
+
+object Navigator {
+    fun launchActivity(
+        context: Context,
+        cls: Class<*>,
+        extraKey: String? = null,
+        extraData: String? = null,
+    ) {
+        Intent(context, cls).also {
+            if (extraKey != null)
+                it.putExtra(extraKey, extraData)
+            context.startActivity(it)
+        }
+    }
+
+    @Throws(NavigationDataException::class)
+    fun getNavigationStringData(activity: Activity, key: String): String {
+        val data = activity.intent.getStringExtra(key)
+        return data ?: throw NavigationDataException("null data")
+    }
+
+    class NavigationDataException(localizedMessage: String = "") : AppException(localizedMessage)
 }
