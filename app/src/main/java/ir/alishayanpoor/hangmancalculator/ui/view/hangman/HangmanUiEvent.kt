a47f6260bd63451bd44dd6114d2ee286bcd78976
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-19 21:31:44 +0430

    Update HangmanUiEvent.kt

--- app/src/main/java/ir/alishayanpoor/hangmancalculator/ui/view/hangman/HangmanUiEvent.kt
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/ui/view/hangman/HangmanUiEvent.kt
@@ -1,5 +1,6 @@
 package ir.alishayanpoor.hangmancalculator.ui.view.hangman
 
 sealed class HangmanUiEvent {
-
+    data class Error(val message: String) : HangmanUiEvent()
+    object GameOver : HangmanUiEvent()
 }
