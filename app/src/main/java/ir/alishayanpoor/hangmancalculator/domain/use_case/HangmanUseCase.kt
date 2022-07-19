9cc37c40809896d1ac7f3620e886c23c19880194
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-19 21:32:30 +0430

    Create next state use case

--- app/src/main/java/ir/alishayanpoor/hangmancalculator/domain/use_case/HangmanUseCase.kt
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/domain/use_case/HangmanUseCase.kt
@@ -1,4 +1,21 @@
 package ir.alishayanpoor.hangmancalculator.domain.use_case
 
+import ir.alishayanpoor.hangmancalculator.exception.AppException
+import ir.alishayanpoor.hangmancalculator.ui.view.hangman.HangmanState
+
 class HangmanUseCase {
+
+    @Throws(AppException::class)
+    fun nextState(currentState: HangmanState): HangmanState {
+        return when (currentState) {
+            HangmanState.S1Init -> HangmanState.S2Rope
+            HangmanState.S2Rope -> HangmanState.S3Head
+            HangmanState.S3Head -> HangmanState.S4Body
+            HangmanState.S4Body -> HangmanState.S5RightHand
+            HangmanState.S5RightHand -> HangmanState.S6LeftHand
+            HangmanState.S6LeftHand -> HangmanState.S7RightFoot
+            HangmanState.S7RightFoot -> HangmanState.S8End
+            HangmanState.S8End -> throw AppException("Cannot go any further")
+        }
+    }
 }
