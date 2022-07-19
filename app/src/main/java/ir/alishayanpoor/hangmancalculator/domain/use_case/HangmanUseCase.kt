8bcfc64353de7beb70915e641f181b6a67519ca6
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-19 21:36:33 +0430

    Add exhaustive to when for compile-time safety

--- app/src/main/java/ir/alishayanpoor/hangmancalculator/domain/use_case/HangmanUseCase.kt
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/domain/use_case/HangmanUseCase.kt
@@ -2,6 +2,7 @@ package ir.alishayanpoor.hangmancalculator.domain.use_case
 
 import ir.alishayanpoor.hangmancalculator.exception.AppException
 import ir.alishayanpoor.hangmancalculator.ui.view.hangman.HangmanState
+import ir.alishayanpoor.hangmancalculator.utils.exhaustive
 
 class HangmanUseCase {
 
@@ -16,6 +17,6 @@ class HangmanUseCase {
             HangmanState.S6LeftHand -> HangmanState.S7RightFoot
             HangmanState.S7RightFoot -> HangmanState.S8End
             HangmanState.S8End -> throw AppException("Cannot go any further")
-        }
+        }.exhaustive
     }
 }
