60b0763448641e47cf97b266b44c7ddafd9124eb
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-19 21:16:36 +0430

    Initialize UI state

--- app/src/main/java/ir/alishayanpoor/hangmancalculator/ui/view/hangman/HangmanUiState.kt
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/ui/view/hangman/HangmanUiState.kt
@@ -5,7 +5,7 @@ data class HangmanUiState(
     private val toSelectNumbers: List<String> = listOf(
         "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
     ),
-    private val selectedNumbers: List<String>,
+    private val selectedNumbers: List<String> = emptyList(),
 )
 
 enum class HangmanState {
