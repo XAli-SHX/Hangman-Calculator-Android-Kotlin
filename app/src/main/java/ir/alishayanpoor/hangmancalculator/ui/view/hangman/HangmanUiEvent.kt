ec293b8ee0984b3d4152826ff37ea2ca77803dd9
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-20 00:06:17 +0430

    Add Win event

--- app/src/main/java/ir/alishayanpoor/hangmancalculator/ui/view/hangman/HangmanUiEvent.kt
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/ui/view/hangman/HangmanUiEvent.kt
@@ -3,4 +3,5 @@ package ir.alishayanpoor.hangmancalculator.ui.view.hangman
 sealed class HangmanUiEvent {
     data class Error(val message: String) : HangmanUiEvent()
     object GameOver : HangmanUiEvent()
+    object Win : HangmanUiEvent()
 }
