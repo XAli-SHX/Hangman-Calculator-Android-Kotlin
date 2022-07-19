3c3703fe0141d89f03e501ee86ed75eb2f25c774
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-19 19:26:23 +0430

    Complete calculateResult method

--- app/src/main/java/ir/alishayanpoor/hangmancalculator/domain/use_case/ArithmeticUseCase.kt
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/domain/use_case/ArithmeticUseCase.kt
@@ -1,7 +1,7 @@
 package ir.alishayanpoor.hangmancalculator.domain.use_case
 
 import ir.alishayanpoor.hangmancalculator.exception.AppException
-import ir.alishayanpoor.hangmancalculator.utils.Constants
+import ir.alishayanpoor.hangmancalculator.utils.Calculator
 import ir.alishayanpoor.hangmancalculator.utils.removeLastChar
 import ir.alishayanpoor.hangmancalculator.utils.removeWhiteSpaces
 
@@ -12,7 +12,7 @@ class ArithmeticUseCase {
         if (newString.length > maxCharSize)
             throw AppException("Reached max char size which is $maxCharSize")
         newChar.forEach { char ->
-            if (getArithmeticList().any { it == char.toString() })
+            if (Calculator.getArithmeticList().any { it == char.toString() })
                 throw AppException("Currently supporting only one arithmetic operator")
         }
         return newString
@@ -20,11 +20,9 @@ class ArithmeticUseCase {
 
     @Throws(AppException::class)
     fun calculateResult(rawText: String): String {
-        val raw = rawText.removeWhiteSpaces()
-        if (rawText.isBlank())
+        if (rawText.removeWhiteSpaces().isBlank())
             throw AppException("There is nothing to calculate")
-
-        return raw
+        return Calculator.calc(rawText)
     }
 
     @Throws(AppException::class)
@@ -32,15 +30,6 @@ class ArithmeticUseCase {
         if (rawText.isEmpty())
             throw AppException("Empty String; Cannot delete it")
         return rawText.removeLastChar()
-    }
-
-    private fun getArithmeticList(): List<String> {
-        return listOf(
-            Constants.CALC_BUTTON_DIV,
-            Constants.CALC_BUTTON_MUL,
-            Constants.CALC_BUTTON_SUB,
-            Constants.CALC_BUTTON_SUM,
-        )
     }
 
     class MaxCharSizeException(private val maxCharSize: Int) : AppException() {
