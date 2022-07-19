63db411ffc314c8dde2348d62d2f60550f8c702a
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-19 16:15:06 +0430

    Create constants for calculator buttons

---
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/utils/Constants.kt
@@ -0,0 +1,21 @@
+package ir.alishayanpoor.hangmancalculator.utils
+
+object Constants {
+    const val CALC_BUTTON_1 = "1"
+    const val CALC_BUTTON_2 = "2"
+    const val CALC_BUTTON_3 = "3"
+    const val CALC_BUTTON_4 = "4"
+    const val CALC_BUTTON_5 = "5"
+    const val CALC_BUTTON_6 = "6"
+    const val CALC_BUTTON_7 = "7"
+    const val CALC_BUTTON_8 = "8"
+    const val CALC_BUTTON_9 = "9"
+    const val CALC_BUTTON_0 = "0"
+
+    const val CALC_BUTTON_DIV = " / "
+    const val CALC_BUTTON_MUL = " * "
+    const val CALC_BUTTON_SUB = " - "
+    const val CALC_BUTTON_SUM = " + "
+
+    const val CALC_BUTTON_CALC = " = "
+}
