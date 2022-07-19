4d0ce15623a8fc5bebbb3327056aeef4a9418302
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-20 00:06:55 +0430

    Change selectedNumbers into  foundedIndexes
    change Map to List

--- app/src/main/java/ir/alishayanpoor/hangmancalculator/ui/view/hangman/HangmanUiState.kt
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/ui/view/hangman/HangmanUiState.kt
@@ -5,7 +5,7 @@ data class HangmanUiState(
     val toSelectNumbers: List<String> = listOf(
         "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
     ),
-    val selectedNumbers: List<String> = emptyList(),
+    val foundedIndexes: List<Int> = emptyList(),
 )
 
 enum class HangmanState {
