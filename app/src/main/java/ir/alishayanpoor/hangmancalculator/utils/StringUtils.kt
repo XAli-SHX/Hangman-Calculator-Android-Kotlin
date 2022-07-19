cee23a54de5eb298bb424af29f85f82224ae9ec4
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-19 18:02:41 +0430

    Add remove last char extension function

---
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/utils/StringUtils.kt
@@ -0,0 +1,15 @@
+package ir.alishayanpoor.hangmancalculator.utils
+
+fun String.removeWhiteSpaces(): String {
+    return this.replace(" ", "")
+}
+
+fun String.removeLastNChars(n: Int): String {
+    require(n >= 0) { "n should be positive" }
+    require(this.length > n) { "string length should be greater than n" }
+    return this.substring(0, this.length - n)
+}
+
+fun String.removeLastChar(): String {
+    return this.removeLastNChars(1)
+}
