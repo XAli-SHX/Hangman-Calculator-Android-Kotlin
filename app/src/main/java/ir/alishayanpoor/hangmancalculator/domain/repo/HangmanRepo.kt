fef823d5bcb59a1dfeeedabfe4a7f97382f11879
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-19 22:45:10 +0430

    Create content description for imges

--- app/src/main/java/ir/alishayanpoor/hangmancalculator/domain/repo/HangmanRepo.kt
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/domain/repo/HangmanRepo.kt
@@ -5,4 +5,5 @@ import ir.alishayanpoor.hangmancalculator.ui.view.hangman.HangmanState
 interface HangmanRepo {
     fun nextState(currentState: HangmanState): HangmanState
     fun getStateImageResourceByCurrentState(currentState: HangmanState): Int
+    fun getStateImageDescriptionByCurrentState(currentState: HangmanState): String
 }
