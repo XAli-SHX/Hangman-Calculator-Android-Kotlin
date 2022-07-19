327ee5c160263e1447a0286fd7dfc96d74ffaee1
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-19 21:16:56 +0430

    Inject hangman use case

--- app/src/main/java/ir/alishayanpoor/hangmancalculator/di/UseCaseModule.kt
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/di/UseCaseModule.kt
@@ -5,10 +5,14 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import ir.alishayanpoor.hangmancalculator.domain.use_case.ArithmeticUseCase
+import ir.alishayanpoor.hangmancalculator.domain.use_case.HangmanUseCase
 
 @Module
 @InstallIn(ViewModelComponent::class)
 class UseCaseModule {
     @Provides
-    fun arithmeticUseCase() = ArithmeticUseCase()
+    fun arithmetic() = ArithmeticUseCase()
+
+    @Provides
+    fun hangman() = HangmanUseCase()
 }
