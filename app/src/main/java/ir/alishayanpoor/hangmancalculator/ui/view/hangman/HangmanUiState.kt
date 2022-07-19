86598739153571fa4d8e5b0f20563942775efee0
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-19 21:12:09 +0430

    Create HangmanUiState.kt

---
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/ui/view/hangman/HangmanUiState.kt
@@ -0,0 +1,13 @@
+package ir.alishayanpoor.hangmancalculator.ui.view.hangman
+
+data class HangmanUiState(
+    private val hangmanState: HangmanState = HangmanState.S1Init,
+    private val toSelectNumbers: List<String> = listOf(
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
+    ),
+    private val selectedNumbers: List<String>,
+)
+
+enum class HangmanState {
+    S1Init, S2Rope, S3Head, S4Body, S5RightHand, S6LeftHand, S7RightFoot, S8End
+}
