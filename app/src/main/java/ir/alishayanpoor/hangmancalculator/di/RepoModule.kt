9186fa1b5a29708c220e911fe33f316429203dee
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-19 22:12:40 +0430

    Specify provide module return type

--- app/src/main/java/ir/alishayanpoor/hangmancalculator/di/RepoModule.kt
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/di/RepoModule.kt
@@ -5,10 +5,11 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import ir.alishayanpoor.hangmancalculator.data.repo.HangmanRepoLocal
+import ir.alishayanpoor.hangmancalculator.domain.repo.HangmanRepo
 
 @Module
 @InstallIn(ViewModelComponent::class)
 class RepoModule {
     @Provides
-    fun hangman() = HangmanRepoLocal()
+    fun hangman(): HangmanRepo = HangmanRepoLocal()
 }
