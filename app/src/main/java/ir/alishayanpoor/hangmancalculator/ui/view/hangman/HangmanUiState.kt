d264fc087e660ce0dddb155c7d7ce02444a1cc8c
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-19 21:32:15 +0430

    Make fields public

--- app/src/main/java/ir/alishayanpoor/hangmancalculator/ui/view/hangman/HangmanUiState.kt
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/ui/view/hangman/HangmanUiState.kt
@@ -1,11 +1,11 @@
 package ir.alishayanpoor.hangmancalculator.ui.view.hangman
 
 data class HangmanUiState(
-    private val hangmanState: HangmanState = HangmanState.S1Init,
-    private val toSelectNumbers: List<String> = listOf(
+    val currentState: HangmanState = HangmanState.S1Init,
+    val toSelectNumbers: List<String> = listOf(
         "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
     ),
-    private val selectedNumbers: List<String> = emptyList(),
+    val selectedNumbers: List<String> = emptyList(),
 )
 
 enum class HangmanState {
