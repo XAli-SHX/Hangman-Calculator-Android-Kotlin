6b768c57813c679d485df5e11c8414e6154f8cbe
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-23 13:43:08 +0430

    Create test for Calculator

---
+++ app/src/test/java/ir/alishayanpoor/hangmancalculator/utils/CalculatorTest.kt
@@ -0,0 +1,122 @@
+package ir.alishayanpoor.hangmancalculator.utils
+
+import com.google.common.truth.Truth
+import org.junit.Test
+import kotlin.math.absoluteValue
+import kotlin.random.Random
+
+class CalculatorTest {
+
+    @Test
+    fun `Calculate raw text with divide, correct result`() {
+        val num1 = Random(121231231).nextInt().absoluteValue
+        val num2 = Random(121231231).nextInt().absoluteValue + 1
+        Truth.assertThat(Calculator.calc("${num1}/${num2}"))
+            .isEqualTo((num1 / num2).toString())
+    }
+
+    @Test
+    fun `Calculate raw text with multiply, correct result`() {
+        val num1 = Random(121231231).nextInt().absoluteValue
+        val num2 = Random(121231231).nextInt().absoluteValue
+        Truth.assertThat(Calculator.calc("${num1}*${num2}"))
+            .isEqualTo((num1 * num2).toString())
+    }
+
+    @Test
+    fun `Calculate raw text with sub, correct result`() {
+        val num1 = Random(121231231).nextInt().absoluteValue
+        val num2 = Random(121231231).nextInt().absoluteValue
+        Truth.assertThat(Calculator.calc("${num1}-${num2}"))
+            .isEqualTo((num1 - num2).toString())
+    }
+
+    @Test
+    fun `Calculate raw text with add, correct result`() {
+        val num1 = Random(121231231).nextInt().absoluteValue
+        val num2 = Random(121231231).nextInt().absoluteValue
+        Truth.assertThat(Calculator.calc("${num1}+${num2}"))
+            .isEqualTo((num1 + num2).toString())
+    }
+
+    @Test
+    fun `Divide Big numbers, correct result`() {
+        val bigNum1: Int = Int.MAX_VALUE / 3
+        val bigNum2: Int = Int.MAX_VALUE / 2
+        assert(
+            Calculator.div(Pair(bigNum1, bigNum2)) == bigNum1 / bigNum2
+        )
+    }
+
+    @Test
+    fun `Multiply Big numbers, correct result`() {
+        val bigNum1: Int = Int.MAX_VALUE / 100000
+        val bigNum2: Int = Int.MAX_VALUE / 100000
+        assert(
+            Calculator.mul(Pair(bigNum1, bigNum2)) == bigNum1 * bigNum2
+        )
+    }
+
+    @Test
+    fun `Sub Big numbers, correct result`() {
+        val bigNum1: Int = Int.MAX_VALUE / 3
+        val bigNum2: Int = Int.MAX_VALUE / 2
+        assert(
+            Calculator.sub(Pair(bigNum1, bigNum2)) == bigNum1 - bigNum2
+        )
+    }
+
+    @Test
+    fun `Add Big numbers, correct result`() {
+        val bigNum1: Int = Int.MAX_VALUE / 3
+        val bigNum2: Int = Int.MAX_VALUE / 2
+        assert(
+            Calculator.add(Pair(bigNum1, bigNum2)) == bigNum1 + bigNum2
+        )
+    }
+
+    @Test
+    fun `Divide numbers, correct result`() {
+        for (i in 0..200) {
+            for (j in 1..200) {
+                assert(
+                    Calculator.div(Pair(i, j)) == i / j
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `Multiply numbers, correct result`() {
+        for (i in 0..200) {
+            for (j in 0..200) {
+                assert(
+                    Calculator.mul(Pair(i, j)) == i * j
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `Sub numbers, correct result`() {
+        for (i in 0..200) {
+            for (j in 0..200) {
+                assert(
+                    Calculator.sub(Pair(i, j)) == i - j
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `Add numbers, correct result`() {
+        for (i in 0..200) {
+            for (j in 0..200) {
+                assert(
+                    Calculator.add(Pair(i, j)) == i + j
+                )
+            }
+        }
+    }
+
+}
