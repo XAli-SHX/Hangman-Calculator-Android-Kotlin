9eb454572c87b20f86dd0d412e3be97c1b3c2456
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-23 18:11:57 +0430

    Update ThousandSeparator.kt

--- app/src/main/java/ir/alishayanpoor/hangmancalculator/utils/ThousandSeparator.kt
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/utils/ThousandSeparator.kt
@@ -1,5 +1,18 @@
 package ir.alishayanpoor.hangmancalculator.utils
 
+import ir.alishayanpoor.logger.Logger
+import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.*
+
 fun String.thousandSeparate(): String {
-    return String.format("%,d", this)
+    return try {
+        Logger.log(this)
+        val formatter = NumberFormat.getInstance(Locale.US) as DecimalFormat
+        formatter.applyPattern("#,###")
+        formatter.format(this.toDouble())
+    } catch (e: Exception) {
+        Logger.log(e.localizedMessage)
+        this
+    }
 }
