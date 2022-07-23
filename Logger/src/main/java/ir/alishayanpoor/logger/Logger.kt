69f538d9cd84e085ef31ba262b1cd41690afe299
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-23 14:22:28 +0430

    Create Logger.kt

---
+++ Logger/src/main/java/ir/alishayanpoor/logger/Logger.kt
@@ -0,0 +1,28 @@
+package ir.alishayanpoor.logger
+
+import android.util.Log
+import com.google.gson.GsonBuilder
+
+object Logger {
+    fun log(message: String?, tag: String = "Ali") {
+        Log.i(tag, message ?: "NULL")
+    }
+
+    fun logDiv(tag: String = "Ali") {
+        Log.i(tag, "----------------------------------------")
+    }
+
+    fun jsonLog(message: Any?, tag: String = "Ali") {
+        log(GsonBuilder().setPrettyPrinting().create().toJson(message), tag)
+    }
+}
+
+fun <T> T.log(tag: String = "Ali"): T {
+    Logger.log(this.toString(), tag)
+    return this
+}
+
+fun <T> T.jsonLog(tag: String = "Ali"): T {
+    Logger.jsonLog(this, tag)
+    return this
+}
