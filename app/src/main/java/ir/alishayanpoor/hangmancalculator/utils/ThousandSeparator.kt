abdf2606d93c4e3c7f30f1516be560bf3fb551bd
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-23 18:31:26 +0430

    Add logs

--- app/src/main/java/ir/alishayanpoor/hangmancalculator/utils/ThousandSeparator.kt
+++ app/src/main/java/ir/alishayanpoor/hangmancalculator/utils/ThousandSeparator.kt
@@ -1,16 +1,14 @@
 package ir.alishayanpoor.hangmancalculator.utils
 
 import ir.alishayanpoor.logger.Logger
+import ir.alishayanpoor.logger.log
 import java.text.DecimalFormat
-import java.text.NumberFormat
-import java.util.*
 
 fun String.thousandSeparate(): String {
     return try {
-        Logger.log(this)
-        val formatter = NumberFormat.getInstance(Locale.US) as DecimalFormat
-        formatter.applyPattern("#,###")
-        formatter.format(this.toDouble())
+        Logger.log("thousandSeparate: init = $this")
+        val formatter = DecimalFormat("#,###")
+        formatter.format(this.toDouble()).log(message = "thousandSeparate: final = ")
     } catch (e: Exception) {
         Logger.log(e.localizedMessage)
         this
